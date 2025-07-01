789bb792185c58c307081b6729b5fef8b4d45dcf
Harry Nguyen <38271443+harrynguyen95@users.noreply.github.com>
2025-07-01 23:02:10 +0700

    Update functions.lua

--- functions.lua
+++ functions.lua
@@ -600,7 +600,7 @@ end
 function setGender()
     if waitImageVisible(what_is_gender) then
         toast("what_is_gender")
-        if waitImageVisible(gender_options) then 
+        if waitImageVisible(gender_options, 10) then 
             math.randomseed(os.time() + math.random())
             local x = math.random(500, 560)
             local y = 440
