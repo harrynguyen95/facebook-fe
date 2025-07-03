27d807cdbc627f68cfa130eb32f6e56bf2157cce
Harry Nguyen <38271443+harrynguyen95@users.noreply.github.com>
2025-07-03 14:19:07 +0700

    Update zClearLastInProgress.lua

--- zClearLastInProgress.lua
+++ zClearLastInProgress.lua
@@ -29,9 +29,3 @@ local function removeLastIfInProgress(filePath)
 end
 
 removeLastIfInProgress(path)
-
--- Respring
-usleep(math.random(1000000, 2000000));
-io.popen('killall -9 SpringBoard');
-usleep(math.random(3000000, 4000000));
-
