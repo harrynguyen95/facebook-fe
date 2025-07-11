59cd21d95b49835b07a33c12a127c4f3f99d16ff
Harry Nguyen <38271443+harrynguyen95@users.noreply.github.com>
2025-07-11 12:09:10 +0700

    up

---
+++ Remote/Xoainfo.lua
@@ -0,0 +1,21 @@
+require(rootDir() .. '/Facebook/utils')
+require(rootDir() .. '/Facebook/functions')
+
+homeAndUnlockScreen()
+
+local dirPath = currentDir() .. "/images/"
+xoainfo_reset_data = {dirPath .. "xoainfo_reset_data.png"}
+xoainfo_info_fake = {dirPath .. "xoainfo_info_fake.png"}
+
+toastr('executeXoaInfo')
+appRun("com.ienthach.XoaInfo")
+sleep(1)
+
+if waitImageVisible(xoainfo_reset_data, 10) then
+    findAndClickByImage(xoainfo_reset_data)
+    sleep(1)
+    
+    if waitImageVisible(xoainfo_info_fake, 30) then
+        toast('Finish...', 5)
+    end
+end
