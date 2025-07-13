5a63bd15c71c4ff5405d2eef349e1d04d6393e4d
Harry Nguyen <38271443+harrynguyen95@users.noreply.github.com>
2025-07-13 12:42:23 +0700

    up

--- utils.lua
+++ utils.lua
@@ -135,7 +135,7 @@ function writeFile(path, data)
 end
 
 function addLineToFile(path, line)
-    (io.open(path, "a")):write(line .. "\n"):close()
+    (io.open(path, "a")):write("\n" .. line):close()
 end
 
 function readFile(path)
