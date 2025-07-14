30f1b44cc935b782b4053bcd4858a1f526e532ac
Harry Nguyen <38271443+harrynguyen95@users.noreply.github.com>
2025-07-14 11:19:50 +0700

    up

--- functions.lua
+++ functions.lua
@@ -170,7 +170,7 @@ function failedCurrentAccount(code)
 end
 
 function saveAccToGoogleForm()
-    if (code == 282 or code == '282') and (info.mailLogin == '' or info.mailLogin == nil) then return nil end
+    if (info.checkpoint == 282 or info.checkpoint == '282') and (info.mailLogin == '' or info.mailLogin == nil) then return nil end
 
     local localIP = readFile(localIPFilePath)
     info.localIP = localIP[#localIP] .. " | " .. LANGUAGE .. " | " .. (REG_PHONE_FIRST and 'phone' or 'mail')
