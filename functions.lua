ecae116085765d671d27da9fb682e0c186cde76f
Harry Nguyen <38271443+harrynguyen95@users.noreply.github.com>
2025-07-25 09:56:34 +0700

    up

--- functions.lua
+++ functions.lua
@@ -554,7 +554,7 @@ end
 function getThuemailConfirmCode()
     if info.thuemailId == nil then return nil end
     
-    sleep(10)
+    sleep(3)
     local tries = 20
     for i = 1, tries do 
         toastr('Call times ' .. i)
